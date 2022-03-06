91b5e324c2d3329164ecb6b0ce5e66f260b265d9
Dennis Ryumin <d.ryumin@sigma-it.ru>
2022-03-06 19:39:05 +0300

    init branch

---
+++ src/com/katapios/Hypot.java
@@ -0,0 +1,17 @@
+package com.katapios;
+
+import java.util.concurrent.Callable;
+
+public class Hypot implements Callable<Double> {
+
+    double side1, side2;
+
+    Hypot(double s1, double s2) {
+        side1 = s1;
+        side2 = s2;
+    }
+
+    public Double call() {
+        return Math.sqrt((side1 * side1) + (side2 * side2));
+    }
+}
